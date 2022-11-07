e12b0ecb113047110e3a7acc69c77f19a0497886
JaviPSanchez <javier.palomino-sanchez@outlook.com>
2022-11-07 23:00:50 +0100

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+// import Featured from "../../components/featured/Featured";
+// import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+// import Footer from "../../components/footer/Footer";
+// import Header from "../../components/header/Header";
+// import MailList from "../../components/mailList/MailList";
+// import Navbar from "../../components/navbar/Navbar";
+// import PropertyList from "../../components/propertyList/PropertyList";
+
+const Home = () => {
+  return (
+    <div>
+      {/* <Navbar />
+      <Header /> */}
+      <div className="">
+        {/* <Featured /> */}
+        <h1 className="text-3xl font-bold text-red-400">
+          Browse by property type
+        </h1>
+        {/* <PropertyList /> */}
+        <h1 className="font-bold">Homes guests love</h1>
+        {/* <FeaturedProperties />
+        <MailList />
+        <Footer /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
